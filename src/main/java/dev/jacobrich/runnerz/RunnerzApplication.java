1c2dcf3660d274b9a7261c6d1e2b4502c97af063
jacob-csci <jacob.rich06@gmail.com>
2024-08-28 16:50:44 -0500

    rest api half complete

--- src/main/java/dev/jacobrich/runnerz/RunnerzApplication.java
+++ src/main/java/dev/jacobrich/runnerz/RunnerzApplication.java
@@ -1,14 +1,34 @@
 package dev.jacobrich.runnerz;
 
+import dev.jacobrich.runnerz.run.Run;
+import dev.jacobrich.runnerz.run.Location;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class RunnerzApplication {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(RunnerzApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzApplication.class, args);
-		//yuh
+
 	}
 
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			Run run = new Run( 1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus( 1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
+			log.info("Run: " + run);
+		};
+	}
 }
---
+++ src/main/java/dev/jacobrich/runnerz/WelcomeMessage.java
@@ -0,0 +1,12 @@
+package dev.jacobrich.runnerz;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeMessage {
+
+    public String getWelcomeMessage(){
+        return "Welcome to the Spring Boot Application";
+
+    }
+}
