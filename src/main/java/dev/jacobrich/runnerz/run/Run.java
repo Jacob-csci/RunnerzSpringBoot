f21de3e4393f91c0b4970cc34bbdde4117b93e0d
Jacob-csci <jacob.rich06@gmail.com>
2024-09-01 21:20:40 -0500

    finshed adding PGSql database plus specific queries

--- src/main/java/dev/jacobrich/runnerz/run/Run.java
+++ src/main/java/dev/jacobrich/runnerz/run/Run.java
@@ -3,13 +3,19 @@ package dev.jacobrich.runnerz.run;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+
 
 public record Run(
+    @Id
     Integer id,
     String title,
     LocalDateTime startedOn,
     LocalDateTime completedOn,
     Integer miles,
-    Location location
+    Location location,
+    @Version
+    Integer version
 ) {}
 
